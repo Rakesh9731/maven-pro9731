f01609f3e99bc25cdfe6460b3a140aafc08d784f
Rakesh9731 <madhu2022manu@gmail.com>
2025-01-07 12:57:58 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello World!,to 2025";
 
     public App() {}
 
